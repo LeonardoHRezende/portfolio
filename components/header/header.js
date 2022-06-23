0b0c389ef7c25af2c582393898b81d8622923761
leonardo-assertech <leonardo-rezende@assertech.app>
2022-06-22 22:35:28 -0300

    First commit

---
+++ components/header/header.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+
+export function Header() {
+  
+    return (
+    <>
+            <h1>
+               Olá 
+            </h1>
+    </>
+  )
+
+}
+
